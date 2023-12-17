bb0bef91995914ca66b6b17b0ef68b880633ae8e
DavidOliveirax <davidoliveirax92@gmail.com>
2023-12-16 21:18:29 -0300

    Att 16/12

---
+++ nuxt-pageDDM/components/header.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex justify-between items-center mt-4">
+        <img class="w-36 ml-36" src="@/assets/image/logo-red.png" alt="Logo DDM Informática">
+        <nav class="mr-36">
+            <lu class="p-4"><NuxtLink to="_about">Quem Somos</NuxtLink></lu>
+            <lu class="p-4"><NuxtLink to="_about">Como comprar</NuxtLink></lu>
+            <lu class="p-4"><NuxtLink to="_about">Prazo de Entrega</NuxtLink></lu>
+            <lu class="p-4"><NuxtLink to="_about">Correios</NuxtLink></lu>
+        </nav>
+    </div>
+</template>
--- nuxt-pageDDM/pages/index.vue
+++ nuxt-pageDDM/pages/index.vue
@@ -1,17 +1,43 @@
 <template>
-    <div>
-        <UContainer>
-    <UCard class="mt-10">
-      <template #header>
-        <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
-          <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
+  <div>
+    <Header />
+  </div>
+  <div class="h-96 flex justify-center items-center">
+    <section>
+      <h1
+        class="bg-gradient-to-r from-gray-400 via-red-400 to-red-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
+        O SEU DESKTOP ESTÁ AQUI!
+      </h1>
+    </section>
+  </div>
+  <div>
+    <UContainer>
+      <UCard class="mt-10">
+        <template #header>
+          <div class="flex justify-between">
+            <h1>Desktop Home</h1>
+            <ColorScheme>
+              <USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" />
+            </ColorScheme>
+          </div>
+        </template>
+        <div class="flex justify-evenly items-center">
+          <div>
+            <img class="w-72 mb-20 " src="@/assets/image/home-i3.png" alt="">
+          </div>
+          <div>
+            <h1 class="pb-5">
+              Desktop HOME i3 <br>
+              Intel i3 4160 / RAM DDR3 8GB <br>
+              SSD 256GB / Placa Mãe H61 <br>
+            </h1>
+            <UButton icon="i-heroicons-arrow-right-circle-solid" to="https://ui.nuxt.com" target="_blank">Comprar
+            </UButton>
+          </div>
         </div>
-      </template>
-      <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">Open Nuxt UI Documentation</UButton>
-    </UCard>
-  </UContainer>
-    </div>
+      </UCard>
+    </UContainer>
+  </div>
 </template>
 
 <script></script>
